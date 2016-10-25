9fb12b83ef1f0fbb10547f9ac5faa7eacc73f2fe
Amyn Remtulla <amynremtulla@amyns-mbp.attlocal.net>
2016-10-25 11:37:16 -0700

    hello World

--- DestroyStuff/ViewController.swift
+++ DestroyStuff/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
